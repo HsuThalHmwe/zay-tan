db24e1d264f881b90ef8edda9a63be5ee5bd1697
HsuThalHmwe <hthucsmb@gmail.com>
2023-04-06 22:35:01 +0630

    feat:fetchdata with query month

--- apps/src/components/PassportApp.tsx
+++ apps/src/components/PassportApp.tsx
@@ -1,10 +1,38 @@
-import exp from "constants";
+import { Box } from "@mui/material";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
+import { useEffect, useState } from "react";
 
 const PassportApps = () => {
+  const [month, setMonth] = useState<number>();
+
+  useEffect(() => {
+    fetchAvailabilty();
+  }, [month]);
+
+  const fetchAvailabilty = async () => {
+    const response = await fetch(
+      `http://localhost:5001/availabilty?months=${month}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Passport App</h1>
-    </div>
+    <Box>
+      <Box sx={{ margin: "0 auto", mt: 2 }}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            disablePast
+            onChange={(value) => {
+              const newMonth = value as Dayjs;
+              setMonth(newMonth.month());
+            }}
+          />
+        </LocalizationProvider>
+      </Box>
+    </Box>
   );
 };
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import cors from "cors";
+import { availability } from "./data";
 
 const app = express();
-const port = 5600;
+const port = 5001;
+
+app.use(cors());
+
+app.get("/availabilty", (req, res) => {
+  //@ts-ignore
+  const selectMonth = parseInt(req.query.months, 10);
+
+  const foundedAvalityInData = availability.filter(
+    (ele) => ele.month === selectMonth
+  );
+  console.log(foundedAvalityInData);
+  res.send(foundedAvalityInData);
+});
 
 app.listen(port, () => {
   console.log("server running", port);
